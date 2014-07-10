94ef99bf0b2633edf4b3ae358c746ea2a31a29f7
Will Dey <52264243+wi11dey@users.noreply.github.com>
2014-07-10 12:10:23 -0400

    Move to Emacs 28
    Turns out glib-networking and gsettings-desktops are not needed for
    Xwidgets in emacs-next-pgtk so something was fixed

--- manifest.scm
+++ manifest.scm
@@ -2,15 +2,13 @@
  '("aspell"
    "aspell-dict-en"
    "dosfstools" ; Contains mkfs.fat.
-   "emacs-xwidgets"
+   "emacs-next"
    "emacs-auctex"
    ;; "emacs-djvu3"
    "emacs-pdf-tools"
    "font-dejavu"
    "font-google-noto"
    "git"
-   "glib-networking" ; For emacs-widgets.
-   "gsettings-desktop-schemas" ; For emacs-widgets.
    "icecat" ; TODO remove
    "parted"
    "perl" ; Magit expects Perl to be installed for some operations.
