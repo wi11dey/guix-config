86fab027d9a2a7b786f6984f6fff84f198cb3cb8
Will Dey <52264243+wi11dey@users.noreply.github.com>
2022-03-05 15:43:48 -0500

    Also build local Emacs src submodule if it exists

--- manifest.scm
+++ manifest.scm
@@ -1,17 +1,25 @@
-(specifications->manifest
- '("aspell"
-   "aspell-dict-en"
-   "dosfstools" ; Contains mkfs.fat.
-   "emacs-next"
-   "emacs-auctex"
-   ;; "emacs-djvu3"
-   "emacs-pdf-tools"
-   "font-dejavu"
-   "font-google-noto"
-   "git"
-   "icecat" ; TODO remove
-   "parted"
-   "perl" ; Magit expects Perl to be installed for some operations.
-   "scrot"
-   "texlive" ; TODO texlive-optex only
-   "unzip"))
+(add-to-load-path ".")
+(use-modules (packages emacs-local)
+	     (gnu package))
+
+(concatenate-manifests
+ (specifications->manifest
+  '("aspell"
+    "aspell-dict-en"
+    "dosfstools" ; Contains mkfs.fat.
+    "emacs-auctex"
+    ;; "emacs-djvu3"
+    "emacs-pdf-tools"
+    "font-dejavu"
+    "font-google-noto"
+    "git"
+    "icecat" ; TODO remove
+    "parted"
+    "perl" ; Magit expects Perl to be installed for some operations.
+    "scrot"
+    "texlive" ; TODO texlive-optex only
+    "unzip"))
+ (packages->manifest
+  (list (if (access? (local-file-absolute-file-name (package-source emacs-local)) R_OK)
+	    emacs-local
+	    emacs-next))))
