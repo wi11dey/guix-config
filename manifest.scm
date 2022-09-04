c3725e853ee30f15d33471ecd107811187c49e51
Will Dey <52264243+wi11dey@users.noreply.github.com>
2022-09-04 17:01:45 -0400

    List manifest format

--- manifest.scm
+++ manifest.scm
@@ -1,24 +1,26 @@
 (use-modules (gnu packages emacs))
 
 (concatenate-manifests
- (specifications->manifest
-  '("aspell"
-    "aspell-dict-en"
-    "dosfstools" ; Contains mkfs.fat.
-    "emacs-auctex"
-    ;; "emacs-djvu3"
-    "emacs-pdf-tools"
-    "font-dejavu"
-    "font-google-noto"
-    "git"
-    "icecat" ; TODO remove
-    "parted"
-    "perl" ; Magit expects Perl to be installed for some operations.
-    "scrot"
-    "texlive" ; TODO texlive-optex only
-    "unzip"))
- (packages->manifest
-  (list (let ((custom-emacs-package (string-append (getenv "HOME") "/.emacs.d/emacs.scm")))
-	  (if (access? custom-emacs-package R_OK)
-	      (load custom-emacs-package)
-	      emacs-next)))))
+ (list
+  (specifications->manifest
+   '("aspell"
+     "aspell-dict-en"
+     "dosfstools" ; Contains mkfs.fat.
+     "emacs-auctex"
+     ;; "emacs-djvu3"
+     "emacs-pdf-tools"
+     "font-dejavu"
+     "font-google-noto"
+     "git"
+     "icecat" ; TODO remove
+     "nyxt"
+     "parted"
+     "perl" ; Magit expects Perl to be installed for some operations.
+     "scrot"
+     "texlive" ; TODO texlive-optex only
+     "unzip"))
+  (packages->manifest
+   (list (let ((custom-emacs-package (string-append (getenv "HOME") "/.emacs.d/emacs.scm")))
+	   (if (access? custom-emacs-package R_OK)
+	       (load custom-emacs-package)
+	       emacs-next))))))
