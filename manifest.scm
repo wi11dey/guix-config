c79a6556e9f4234539741f74b8d8a4a99fa1adf6
Will Dey <52264243+wi11dey@users.noreply.github.com>
2022-09-30 20:17:42 -0400

    Set manifest to always use emacs-next

--- manifest.scm
+++ manifest.scm
@@ -1,26 +1,18 @@
-(use-modules (gnu packages emacs))
-
-(concatenate-manifests
- (list
-  (specifications->manifest
-   '("aspell"
-     "aspell-dict-en"
-     "dosfstools" ; Contains mkfs.fat.
-     "emacs-auctex"
-     ;; "emacs-djvu3"
-     "emacs-pdf-tools"
-     "font-dejavu"
-     "font-google-noto"
-     "git"
-     "icecat" ; TODO remove
-     "nyxt"
-     "parted"
-     "perl" ; Magit expects Perl to be installed for some operations.
-     "scrot"
-     "texlive" ; TODO texlive-optex only
-     "unzip"))
-  (packages->manifest
-   (list (let ((custom-emacs-package (string-append (getenv "HOME") "/.emacs.d/emacs.scm")))
-	   (if (access? custom-emacs-package R_OK)
-	       (load custom-emacs-package)
-	       emacs-next))))))
+(specifications->manifest
+ '("aspell"
+   "aspell-dict-en"
+   "dosfstools" ; Contains mkfs.fat.
+   "emacs-auctex"
+   ;; "emacs-djvu3"
+   "emacs-next"
+   "emacs-pdf-tools"
+   "font-dejavu"
+   "font-google-noto"
+   "git"
+   "icecat" ; TODO remove
+   "nyxt"
+   "parted"
+   "perl" ; Magit expects Perl to be installed for some operations.
+   "scrot"
+   "texlive"
+   "unzip"))
