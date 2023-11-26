fa1ab2750e4e0d0ea5dc24e6f2096b06765d737c
Will Dey <52264243+wi11dey@users.noreply.github.com>
2023-11-26 13:54:53 -0500

    finished packaging pdfsandwich

--- packages/pdfsandwich.scm
+++ packages/pdfsandwich.scm
@@ -1,9 +1,18 @@
 (define-module (packages pdfsandwich)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages ghostscript)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages ocr)
+  #:use-module (gnu packages pdf)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages sphinx)
   #:use-module (gnu packages video)
   #:use-module (guix build-system meson)
   #:use-module (guix build-system ocaml)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix svn-download)
   #:use-module (guix packages))
@@ -18,7 +27,7 @@
 		    (url "https://github.com/unpaper/unpaper")
 		    (commit (string-append "unpaper-" version))))
 	      (sha256
-               (base32 "1by8rmbva8mfrivdbbkr2gx4kga89zqygkd4cfjl76nr8mdcdamb"))))
+               (base32 "1a9cqiq458si4mv56gpm4rw4qb50f1psszf9m8007m3lrn95f4wa"))))
     (build-system meson-build-system)
     (home-page "https://www.flameeyes.eu/projects/unpaper")
     (synopsis "A post-processing tool for scanned sheets of paper. ")
@@ -26,7 +35,10 @@
     (license (list license:gpl2+
 		   license:expat
 		   license:asl2.0))
-    (native-inputs (list python-sphinx))
+    (native-inputs (list pkg-config
+			 python-sphinx
+			 python-pillow
+			 python-pytest))
     (inputs (list ffmpeg-5))))
 
 (define-public pdfsandwich
@@ -37,13 +49,35 @@
 	      (method svn-fetch)
 	      (uri (svn-reference
 		    (url "svn://svn.code.sf.net/p/pdfsandwich/code/trunk/src")
-		    (revision "75")))
+		    (revision 75)))
 	      (sha256
 	       (base32 "1420c33divch087xrr61lvyf975bapqkgjqaighl581i69nlzsm6"))))
     (build-system ocaml-build-system)
     (home-page "http://www.tobias-elze.de/pdfsandwich/")
     (synopsis "A tool to make \"sandwich\" OCR pdf files ")
     (description "pdfsandwich generates \"sandwich\" OCR pdf files, i.e. pdf files which contain only images (no text) will be processed by optical character recognition (OCR) and the text will be added to each page invisibly \"behind\" the images. ")
-    (license license:gpl2+)))
+    (license license:gpl2+)
+    (native-inputs (list perl))
+    (inputs (list ghostscript
+		  imagemagick
+		  poppler
+		  tesseract-ocr
+		  unpaper))
+    (arguments
+     (list
+      #:tests? #f ; No tests included.
+      #:configure-flags #~(list (string-append "--prefix=" #$output))
+      #:phases
+      #~(modify-phases %standard-phases
+	  (add-after 'install 'wrap-program
+	    (lambda _
+	      (wrap-program (string-append #$output "/bin/pdfsandwich")
+		`("PATH" ":" prefix
+		  ,(map (lambda (input) (string-append input "/bin"))
+			'(#$ghostscript
+			  #$imagemagick
+			  #$poppler
+			  #$tesseract-ocr
+			  #$unpaper)))))))))))
 
-unpaper
+pdfsandwich
