6044ee9e168705c0371221f56e00f8e782f004bb
Will Dey <52264243+wi11dey@users.noreply.github.com>
2023-11-27 22:11:31 -0500

    autohint any package with otf fonts

--- manifest.scm
+++ manifest.scm
@@ -1,7 +1,43 @@
 (use-modules (gnu packages emacs)
              (gnu packages fonts)
              (gnu packages fontutils)
-             (guix packages))
+             (gnu packages tex)
+             (guix packages)
+             (guix utils))
+
+(define (autohint otf-font)
+  (package
+    (inherit otf-font)
+    (name (string-append (package-name otf-font) "-ttf"))
+    (native-inputs
+     (cons*
+      fontforge
+      ttfautohint
+      (package-native-inputs otf-font)))
+    (arguments
+     (substitute-keyword-arguments (package-arguments otf-font)
+       ((#:phases phases)
+        #~(append #$phases
+                  ;; Steps from https://tex.stackexchange.com/a/201798:
+                  `((ttfautohint . ,(lambda _
+                                      (let* ((fonts (string-append #$output "/share/fonts"))
+                                             (source (string-append fonts "/opentype"))
+                                             (destination (string-append fonts "/truetype")))
+                                        (mkdir destination)
+                                        (for-each (lambda (otf)
+                                                    (let* ((base (string-append "/" (basename otf ".otf")))
+                                                           (unhinted (string-append destination base "-unhinted.ttf"))
+                                                           (ttf (string-append destination base ".ttf")))
+                                                      (invoke #+(file-append fontforge "/bin/fontforge")
+                                                              "-lang=ff"
+                                                              "-c" "Open($1); Generate($2)"
+                                                              otf unhinted)
+                                                      (delete-file otf)
+                                                      (invoke #+(file-append ttfautohint "/bin/ttfautohint")
+                                                              unhinted ttf)
+                                                      (delete-file unhinted)))
+                                                  (find-files source "\\.otf$"))
+                                        (rmdir source)))))))))))
 
 (concatenate-manifests
  (list
